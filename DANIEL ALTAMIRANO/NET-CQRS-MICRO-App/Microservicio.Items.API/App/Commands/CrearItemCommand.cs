bb64800e3fbc590bb6277304bd97f07e4ea4f132
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2025-09-24 16:23:05 -0500

    Create CrearItemCommand.cs

---
+++ DANIEL ALTAMIRANO/NET-CQRS-MICRO-App/Microservicio.Items.API/App/Commands/CrearItemCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Microservicio.Items.API.Application.Commands
+{
+    public record CrearItemCommand(
+        string   Titulo, 
+        string   Descripcion, 
+        DateTime FechaEntrega, 
+        byte     Relevancia
+    ) : IRequest<int>;
+}
