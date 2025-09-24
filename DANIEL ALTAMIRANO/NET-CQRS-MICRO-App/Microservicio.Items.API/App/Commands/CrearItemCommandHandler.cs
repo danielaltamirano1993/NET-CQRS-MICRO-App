a3c591b462104ed3bf3fda19a0dacaa4cbeb068e
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2025-09-24 16:23:08 -0500

    Create CrearItemCommandHandler.cs

---
+++ DANIEL ALTAMIRANO/NET-CQRS-MICRO-App/Microservicio.Items.API/App/Commands/CrearItemCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microservicio.Items.API.Domain;
+using Microservicio.Items.API.Infrastructure;
+
+namespace Microservicio.Items.API.Application.Commands
+{
+    public class CrearItemCommandHandler : IRequestHandler<CrearItemCommand, int>
+    {
+        private readonly ItemDbContext _context;
+
+        public CrearItemCommandHandler(
+            ItemDbContext context
+        ) => _context = context;
+
+        public async Task<int> Handle(
+            CrearItemCommand request, 
+            CancellationToken cancellationToken
+        ) 
+        {
+            var item = new ItemTrabajo
+            {
+                Titulo        = request.Titulo,
+                Descripcion   = request.Descripcion,
+                FechaCreacion = DateTime.UtcNow,
+                FechaEntrega  = request.FechaEntrega,
+                Relevancia    = request.Relevancia,
+                Estado        = "Pendiente"
+            };
+
+            _context.ItemTrabajo.Add(item);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return item.ItemId;
+        }
+    }
+}
