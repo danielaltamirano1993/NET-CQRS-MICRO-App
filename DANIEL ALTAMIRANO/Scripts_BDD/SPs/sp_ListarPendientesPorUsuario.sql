986b74b3fc5590b31c421fea3a1aaf6720c43673
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2025-09-24 16:24:23 -0500

    Create sp_ListarPendientesPorUsuario.sql

---
+++ DANIEL ALTAMIRANO/Scripts_BDD/SPs/sp_ListarPendientesPorUsuario.sql
@@ -0,0 +1,30 @@
+
+USE BDD_Items;
+GO
+
+IF OBJECT_ID('dbo.sp_ListarPendientesPorUsuario', 'P') IS NOT NULL
+-- Procedimiento para listar pendientes por usuario
+    DROP PROCEDURE dbo.sp_ListarPendientesPorUsuario;
+GO
+
+CREATE 
+OR ALTER PROCEDURE dbo.sp_ListarPendientesPorUsuario
+    @UsuarioId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT   ItemId, 
+		     Titulo, 
+		     Descripcion, 
+		     FechaEntrega, 
+		     Relevancia, 
+		     Estado
+    FROM     ItemTrabajo
+    WHERE	 UsuarioAsignado = @UsuarioId 
+	AND		 Estado <> 'Completado'
+    ORDER BY FechaEntrega 
+	ASC, 
+	Relevancia DESC;
+END
+GO
