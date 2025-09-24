53d1b9ceb77272e240b1ae458ec8e42f3296c772
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2025-09-24 16:23:15 -0500

    Create GetPendientesPorUsuarioQuery.cs

---
+++ DANIEL ALTAMIRANO/NET-CQRS-MICRO-App/Microservicio.Items.API/App/Queries/GetPendientesPorUsuarioQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microservicio.Items.API.Domain;
+using System.Collections.Generic;
+
+namespace Microservicio.Items.API.Application.Queries
+{
+    public record GetPendientesPorUsuarioQuery(
+        int UsuarioId
+    ) : IRequest<IEnumerable<ItemTrabajo>>;
+}
